1e3c9e3d36152279562caf1f75bd7458aeb11dfb
kdncode <kdncode@gmail.com>
2018-05-31 18:25:51 -0500

    add Product page

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
+    constructor(props) {
+        super(props);
+        this.state = ({
+            editing : false
+        })
+    }
+
+    renderNormal = () => {
+        return  <div>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+    }
+
+    renderForm = () => {
+        return <div>
+                    <button>Save</button> 
+               </div>
+    }
+
+    show_button = () => {
+        if (this.state.editing === false) {
+            return this.renderNormal();
+        } else {
+            return this.renderForm();
+        }
+    }
+    
     render() {
         return (
             <div>
                 My Product <br/>
-                <button>Edit</button>
-                <button>Delete</button>
+                {this.show_button()}
             </div>
         );
     }
