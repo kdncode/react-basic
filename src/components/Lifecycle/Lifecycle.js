612cc8a0f3a17693d8e5e2d75bb096e40e58cafb
kdncode <kdncode@gmail.com>
2018-05-31 21:45:22 -0500

    update lifecycle (State)

--- src/components/Lifecycle/Lifecycle.js
+++ src/components/Lifecycle/Lifecycle.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class Lifecycle extends Component {
     constructor(props) {
         super(props);
+        console.log('Init')
         this.state = {
             component: 'Component Init'
         };
     }
 
-    
     componentWillMount() {
         this.setState({ component: 'Will'});
     }
@@ -16,14 +16,34 @@ class Lifecycle extends Component {
     componentDidMount() {
         console.log('Did');
     }
+
+    updateState = () => {
+        this.setState({ component: 'New State'})
+    }
+
+    // Return False : Stop at ShouldUpdate or True
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('should update ' + nextState);
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('Will update ' + nextState);
+    }
     
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Did update ' + prevState);
+    }
     
     
     render() {
+        console.log('Render')
         console.log(this.state.component);
         return (
-            <div>
-                Lifecycle
+            
+            <div><br/>
+                <button onClick={ () => { this.updateState() }}>Update Lifecycle</button>
             </div>
         );
     }
